8dc7de907d6cb0da54dca4626b61c1cbf8a4b399
Your Name <wunnahtun@blue-stone.net>
2024-06-20 08:11:28 +0630

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BookStore',
+  webDir: 'www'
+};
+
+export default config;
